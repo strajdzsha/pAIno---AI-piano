c31c719cfb05fa380063cc552af61dbf882507a8
Teodora Srećković <teodorasrec@gmail.com>
2022-08-04 16:24:33 +0200

    Modified load_dataset

--- midi_conversion/load_dataset.py
+++ midi_conversion/load_dataset.py
@@ -1,9 +1,6 @@
-#%%
-
 import pandas as pd
 from mido import MidiFile
 import muspy
-import torch
 import numpy as np
 
 path = 'D:/PSIML/datasets/maestro-v3.0.0/'
@@ -14,23 +11,11 @@ print(len(filenames))
 
 dataset = []
 
-i = 0
 for fname in filenames:
     music = muspy.read_midi(path + fname, backend='pretty_midi')
     music_pitch = muspy.to_pitch_representation(music, use_hold_state=True, dtype=int)
     dataset.append(music_pitch.squeeze())
-    print(music_pitch.shape)
-    #np.concatenate(dataset, music_pitch.squeeze())
-    #dataset.append
-    
-    if i >= 10: break
-    i+=1
+    #print(music_pitch.shape)
 
-#%%
-#dataset_np = np.array(dataset, dtype=int)
-#new_dataset = np.array(dataset, dtype=np.ndarray).flatten()
-#print(new_dataset)
-# %%
 new_dataset = np.concatenate(dataset, axis=0)
 print(new_dataset.shape)
-# %%
